d58dce99f9ee6e1e05d90a56b59e989e8311cb97
GeorgeCsd <giorgostg7@yahoo.com>
2023-03-07 14:05:41 +0200

    restapp not working yet but we insert objects in db via EntityManager

---
+++ Chapter15/target/generated-sources/annotations/org/example/Book_.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.0.v20130507-rNA", date="2023-03-07T14:00:29")
+@StaticMetamodel(Book.class)
+public class Book_ { 
+
+    public static volatile SingularAttribute<Book, Boolean> illustrations;
+    public static volatile SingularAttribute<Book, Float> price;
+    public static volatile SingularAttribute<Book, String> isbn;
+    public static volatile SingularAttribute<Book, String> description;
+    public static volatile SingularAttribute<Book, String> id;
+    public static volatile SingularAttribute<Book, String> title;
+    public static volatile SingularAttribute<Book, Integer> nbOfPage;
+
+}
